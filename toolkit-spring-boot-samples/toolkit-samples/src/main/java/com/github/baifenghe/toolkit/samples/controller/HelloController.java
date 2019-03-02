795980e611bdc506771e3d4f78310617595cb583
baifenghe <1170126481@qq.com>
2019-03-02 12:43:38 +0800

    :white_check_mark: update test cases

--- toolkit-spring-boot-samples/toolkit-samples/src/main/java/com/github/baifenghe/toolkit/samples/controller/HelloController.java
+++ toolkit-spring-boot-samples/toolkit-samples/src/main/java/com/github/baifenghe/toolkit/samples/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.github.baifenghe.toolkit.samples.controller;
 import com.github.baifenghe.toolkit.annotation.ParamsCheck;
 import com.github.baifenghe.toolkit.annotation.ParamsCheck4Json;
 import com.github.baifenghe.toolkit.common.util.ResponseHelper;
+import com.github.baifenghe.toolkit.samples.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,8 @@ public class HelloController {
 
     @RequestMapping("json")
     @ParamsCheck4Json(params = "name")
-    public ResponseEntity test(@RequestBody String name) {
-        String body = "ParamsCheck4Json操作成功！name is " + name;
+    public ResponseEntity test(@RequestBody User user) {
+        String body = "ParamsCheck4Json操作成功！name is " + user.getName();
         return ResponseHelper.ok().body(body);
     }
 
---
+++ toolkit-spring-boot-samples/toolkit-samples/src/main/java/com/github/baifenghe/toolkit/samples/entity/User.java
@@ -0,0 +1,23 @@
+package com.github.baifenghe.toolkit.samples.entity;
+
+import lombok.Data;
+
+/**
+ * User
+ *
+ * @author bfh
+ * @since 2019/03/02
+ */
+@Data
+public class User {
+
+    /**
+     * 用户ID
+     */
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String name;
+}
